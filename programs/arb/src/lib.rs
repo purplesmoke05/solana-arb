234aaba399a7475c269fb0218697cc2e1a15e4fd
purplehaze <yosuke.otosu@gmail.com>
2022-08-23 22:29:35 +0900

    update jup.ag to v3

--- programs/arb/src/lib.rs
+++ programs/arb/src/lib.rs
@@ -1,4 +1,4 @@
 use anchor_lang::prelude::declare_id;
 anchor_gen::generate_cpi_crate!("jup-ag.json");
 
-declare_id!("JUP2jxvXaqu7NQY1GmNF4m1vodw12LVXYxbFL2uJvfo");
+declare_id!("JUP3c2Uh3WA4Ng34tw6kPd2G4C5BB21Xo36Je1s32Ph");
